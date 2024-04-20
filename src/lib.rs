10dba5075105eaa8624ec92b673a23b0e0b47102
DoctorNasa <Roongroj777@gmail.com>
2024-04-20 23:14:55 +0700

    create READMe

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::{cmp::Ordering, io::BufRead};
 
 use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -90,5 +91,27 @@ fn test_prices_and_volume() {
     let mut order_book = OrderBook::new();
 
     // Create some buy orders.
-    let Order::new(dec!(35), dec!(690), BuyOrSell::Buy);
+    let buy_order_1 = Order::new(dec!(35), dec!(690), BuyOrSell::Buy);
+    let buy_order_2 = Order::new(dec!(20), dec!(685), BuyOrSell::Buy);
+    let buy_order_3 = Order::new(dec!(15), dec!(690), BuyOrSell::Buy);
+
+    // Create some sell orders.
+    let sell_order_1 = Order::new(dec!(10), dec!(700), BuyOrSell::Sell);
+    let sell_order_2 = Order::new(dec!(25), dec!(705), BuyOrSell::Sell);
+    let sell_order_3 = Order::new(dec!(30), dec!(700), BuyOrSell::Sell);
+
+    // Add the orders to the order_book
+    order_book.add_order_to_orderbook(buy_order_1);
+    order_book.add_order_to_orderbook(buy_order_2);
+    order_book.add_order_to_orderbook(buy_order_3);
+    order_book.add_order_to_orderbook(sell_order_1);
+    order_book.add_order_to_orderbook(sell_order_2);
+    order_book.add_order_to_orderbook(sell_order_3);
+
+    assert_eq!(order_book.best_buy_price().unwrap(), dec!(690));
+    assert_eq!(order_book.best_sell_price().unwrap(), dec!(700));
+    // Total Buying Order Quantity = 35+20+15
+    assert_eq!(order_book.buy_volume().unwrap(), dec!(70));
+    // Total Selling Order Quantity = 10+25+30
+    assert_eq!(order_book.sell_volume().unwrap(), dec!(65));
 }
